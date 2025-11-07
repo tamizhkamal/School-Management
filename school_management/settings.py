0a99ea6b5d50d12c491d460a85c53fa75354295c
tamizh_kamal <tamizhkamal6590@gmail.com>
2025-11-07 19:02:08 +0530

    Add gunicorn for deployment jkg

--- school_management/settings.py
+++ school_management/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = 'django-insecure-g3o99a435lf6=xr0nb@v%5sz74+e4&#dzr_u^n0bentbkvu+va
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['school-management-b5ce.onrender.com', 'localhost', '127.0.0.1']
+
 
 
 # Application definition
